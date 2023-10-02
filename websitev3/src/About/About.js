c69a9d07d49c245f65c7d7d43b23d34b04013873
patteg21 <patteg21@gmail.com>
2023-10-02 16:43:45 -0400

    added map to about

--- websitev3/src/About/About.js
+++ websitev3/src/About/About.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 
+import Map from "./Map";
+
+
 // https://www.copycat.dev/blog/react-draggable/#:~:text=const%20Draggable%20%3D%20require(%20'react,Draggable%2F%3E%20component%20around%20it.
 import Draggable, {DraggableCore} from "react-draggable";
 
@@ -7,21 +10,15 @@ import Draggable, {DraggableCore} from "react-draggable";
 import { ResizableBox } from 'react-resizable';
 
 
+import background from "./blackhole2.png"
 
 import "./about.css"
 
 export default function About(){
-    
+
     return(
         <div className="About">
-            <Draggable>
-            <ResizableBox>
-                <div className="Draggable About-Header">
-                    <h1>About</h1>
-                </div>
-            </ResizableBox>
-            </Draggable>
-
+            <Map />
         </div>
     )
 }
---
+++ websitev3/src/About/Map.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+
+//https://www.npmjs.com/package/dotted-map
+import DottedMap from 'dotted-map';
+
+export default function Map(){
+    const[mapLoading, setMapLoading] = useState(true)
+    const[createMap, setCreateMap] = useState(null)
+
+
+    
+    useEffect(()=>{
+
+        let color ="rgb(142, 5, 194)"
+
+        const map = new DottedMap({ height: 75, grid: 'diagonal' });
+
+        //London
+        map.addPin({
+            lat: 51.5072,
+            lng: 0.1276,
+            svgOptions: { color: color, radius: 0.4 },
+        });
+        // New York
+        map.addPin({
+            lat: 40.73061,
+            lng: -73.935242,
+            svgOptions: { color: color, radius: 0.4 },
+        });
+        //Charlotte
+        map.addPin({
+            lat: 35.2271,
+            lng: -80.8431,
+            svgOptions: { color: color, radius: 0.4 },
+        });
+        // Seattle
+        map.addPin({
+            lat: 47.6061,
+            lng: -122.3328,
+            svgOptions: { color: color, radius: 0.4 },
+        });
+    
+        const svgMap = map.getSVG({
+            radius: 0.22,
+            color: '#9B938B',
+            shape: 'circle',
+            backgroundColor: 'transparent',
+        });
+        setCreateMap(svgMap)
+        setMapLoading(false)
+    }, [])
+
+
+    return(
+        <div className="Map">
+            <div className="WorldMap">
+            { mapLoading ? <h1>Loading...</h1> : <img src={`data:image/svg+xml;utf8,${encodeURIComponent(createMap)}`} />}
+            </div>
+        </div>
+    )
+}
