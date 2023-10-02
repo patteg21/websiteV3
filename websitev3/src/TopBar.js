24a6757a3e46b7087780f9bcf8e2a3897760f640
patteg21 <patteg21@gmail.com>
2023-10-02 08:29:07 -0400

    added dropdown

--- websitev3/src/TopBar.js
+++ websitev3/src/TopBar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Arrow from "./down-arrow.svg"
+
 export default function TopBar(){
 
     return(
         <div className="TopBar">
             <ul>
-                <Link className="NavLink" to="/"><li>Home</li></Link>
-                <Link className="NavLink" to="/about"><li>About</li></Link>
-                <Link className="NavLink" to="/resume"><li>Resume</li></Link>
+                <Link className="NavLink" to="/"><li>Home<img src={Arrow}></img></li></Link>
+                <Link className="NavLink" to="/about"><li>About<img src={Arrow}></img></li></Link>
+                <Link className="NavLink" to="/resume"><li>Resume<img src={Arrow}></img></li></Link>
             </ul>
         </div>
     )
