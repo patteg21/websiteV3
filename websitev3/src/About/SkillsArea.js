9fedb449f4542aee164dd37d16e00281586bfb92
patteg21 <patteg21@gmail.com>
2023-10-04 16:37:21 -0400

    added skills section

--- websitev3/src/About/SkillsArea.js
+++ websitev3/src/About/SkillsArea.js
@@ -3,8 +3,29 @@ import React from "react";
 export default function SkillsArea(){
 
     return(
-        <div className="SkillsArea">
-            Hello
+        <div className="SkillsArea grid grid-cols-2 gap-4">
+            <ul className="">
+                <li>Python</li>
+                <li>JavaScript</li>
+                <li>C++</li>
+            </ul>
+            <ul className="">
+                <li>MongoDB</li>
+                <li>SQL</li>
+                <li>MySQL</li>
+            </ul>
+            <ul className="">
+                <li>Django</li>
+                <li>Flask</li>
+                <li>Express</li>
+                <li>Node.js</li>
+            </ul>
+            <ul className="">
+                <li>React</li>
+                <li>Three.js</li>
+                <li>Tailwind</li>
+                <li>Blender</li>
+            </ul>
         </div>
     )
 }
