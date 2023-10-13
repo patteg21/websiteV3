6cfd798c71760df68d5a6fa91473c17407f20a2c
patteg21 <patteg21@gmail.com>
2023-10-13 00:24:28 -0400

    changed icon

--- websitev3/src/About/FunFacts.js
+++ websitev3/src/About/FunFacts.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FunFacts(){
 
-    const chessFact = <div>YO</div>
+    // const chessFact = <div>YO</div>
 
     return(
         <div className="FunFacts">
